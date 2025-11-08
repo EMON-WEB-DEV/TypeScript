140d8800bc60769a4e78f7bdcaa97859a73d80af
EMON <jakariachowdhury2427@gmail.com>
2025-11-09 00:56:47 +0600

    first commit

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Pet = "My Name Is Emon";
+console.log(Pet);
+let age = 22;
+age = 23;
+let x = "hasi 12";
+x = 23;
+let y = true;
+y = false;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+const Pet: string = "My Name Is Emon";
+
+console.log( Pet);
+ 
+let age: number =22;
+
+age =23;
+
+let x: any = "hasi 12";
+
+x= 23;
+
+
+
+let y : boolean= true;
+y = false;
