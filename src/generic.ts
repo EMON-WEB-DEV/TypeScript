7e3ce5c8c6a15b33997343a92e3387aeabeb92af
EMON <jakariachowdhury2427@gmail.com>
2025-11-11 01:29:49 +0600

    Add generic type definition and person details array

---
+++ src/generic.ts
@@ -0,0 +1,19 @@
+type Generic <T> = Array<T>;
+  
+const personDetails : Generic <{name:string; roll:number; id:number}> = 
+
+
+[
+
+           {name:'Emon',
+          roll: 145,
+          id :12,
+        
+        },
+
+        {
+                name : "Hashi",
+                roll : 3,
+                id : 14,
+        }
+]
