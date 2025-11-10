ab4e8b6d93be12a2b1f99b6bf87c1ac5404bcd48
EMON <jakariachowdhury2427@gmail.com>
2025-11-11 01:21:22 +0600

    Add method to display person details in the person class

--- src/class.ts
+++ src/class.ts
@@ -10,10 +10,19 @@ class person {
                 this.age = age;
                 this.id = id;
          }
+         use(){
+           
+          console.log(`My Name is ${this.name},My Age is ${this.age},And My Is is ${this.id}`);
+                
+         }
 
 }
-const person1 = new person ('Emon',22,123);
-const person2 = new person ('Jony',24,124);
+const person1  = new person ('Emon',22,123);
+const person2 = new person ('Hashi',24,124);
+const person3 = new person (' Sza',2,12);
 
+person1.use();
+person2.use();
+person3.use();
 
-console.log(person1,person2);
+//console.log(person1,person2);
